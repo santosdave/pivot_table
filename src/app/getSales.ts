1677117b0b0ab39bb7eb6a07fe6f901eb20f9f30
santos dave <santosdave86@gmail.com>
2021-08-13 11:13:39 +0300

    implementing fetch api

--- src/app/getSales.ts
+++ src/app/getSales.ts
@@ -1,3 +1,4 @@
+/* import React, {useEffect,useState} from "react"; */
 import {Sales} from "./Sales"
 import sales from "./sales.json";
 
@@ -7,5 +8,25 @@ import sales from "./sales.json";
  * @returns A promise that resolves with the orders.
  */
 export async function getSales(): Promise<Sales[]> {
+  /**
+ * Retrieves the sales to aggregate in the pivot table.
+ *
+ * @returns A promise that resolves with the orders.
+ */
+
+
+  /* const url="https://drive.google.com/file/d/1bTfHnEUAo6VrJQTX8uJTl_VACioUNcbt/view";
+  const [sales, setSales]=useState([]);
+  useEffect(() =>{
+    const fetchData=async ()=>{
+      const res=await fetch(url);
+      const json= await res.json();
+      setSales(json.hits)
+    };
+    fetchData();
+  },[setSales]);
+  function async() {
+    throw new Error("Function not implemented.");
+  } */
   return Promise.resolve(sales);
 }
